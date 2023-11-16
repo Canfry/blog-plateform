25739bb32cda5056dbf5f0828cf915c0fb30a2dc
Christophe anfry <frybcn@gmail.com>
2023-11-16 21:51:52 +0100

    Set up sendEmail adding sendgrid

--- variables.py
+++ variables.py
@@ -4,5 +4,6 @@ from datetime import datetime
 USERNAME = os.environ.get('EMAIL')
 PASSWORD = os.environ.get('PASSWORD')
 DEST_EMAIL = os.environ.get('DEST_EMAIL')
+MY_EMAIL = os.environ.get('MY_EMAIL')
 
 today = datetime.now().strftime("%B %d, %Y")
